72b5e777cc5e69861a75a0eed36c7ab92f0cbc35
duafnso <145731071+duafnso@users.noreply.github.com>
2024-12-05 22:07:16 -0300

    Update ConteudoInvalidoException.java

--- ConteudoInvalidoException.java
+++ ConteudoInvalidoException.java
@@ -1,4 +1,4 @@
-// Nome: Seu Nome
+// Jose Eduardo Afonso - 2151323
 public class ConteudoInvalidoException extends RuntimeException {
     public ConteudoInvalidoException(String mensagem) super(mensagem);
     }
